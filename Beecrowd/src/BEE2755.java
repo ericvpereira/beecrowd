6bf0d70da616b88476a54a49e036e7c9532323b9
Eric Vieira Pereira <86498398+ericvpereira@users.noreply.github.com>
2025-10-15 22:00:57 -0300

    Add files via upload

---
+++ Beecrowd/src/BEE2755.java
@@ -0,0 +1,8 @@
+public class BEE2755 {
+    public static void main(String[] args) {
+        System.out.println("\"Ro'b'er\tto\\/\"");     // aqui o \t, e a barra invertida antes da barra normal
+        System.out.println("(._.) ( l: ) ( .-. ) ( :l ) (._.)");
+        System.out.println("(^_-) (-_-) (-_^)"); 
+        System.out.println("(\"_\") ('.')");
+    }
+}
