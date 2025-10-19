fd55f17fb4f681b7b46b0cacb3fdbda0ac3d1152
Eric Vieira Pereira <86498398+ericvpereira@users.noreply.github.com>
2025-10-19 20:24:07 -0300

    Add files via upload

--- Beecrowd/src/BEE1061.java
+++ Beecrowd/src/BEE1061.java
@@ -3,18 +3,43 @@ import java.util.Scanner;
 public class BEE1061 {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
-        int diaInicio = sc.nextInt();
-        int horaInicio = sc.nextInt();
-        int minutoInicio = sc.nextInt();
-        int segundoInicio = sc.nextInt();
-        int diaFim = sc.nextInt();
-        int horaFim = sc.nextInt();
-        int minutoFim = sc.nextInt();
-        int segundoFim = sc.nextInt();
+        String[] dataInicio = sc.nextLine().split(" ");
+        int dia1 = Integer.parseInt(dataInicio[1]);
 
+        String[] tempoInicio = sc.nextLine().split(" : ");
+        int h1 = Integer.parseInt(tempoInicio[0]);
+        int m1 = Integer.parseInt(tempoInicio[1]);
+        int s1 = Integer.parseInt(tempoInicio[2]);
 
+        String[] diaFim = sc.nextLine().split(" ");
+        int dia2 = Integer.parseInt(diaFim[1]);
+
+        String[] tempoFim = sc.nextLine().split(" : ");
+        int h2 = Integer.parseInt(tempoFim[0]);
+        int m2 = Integer.parseInt(tempoFim[1]);
+        int s2 = Integer.parseInt(tempoFim[2]);
+
+        int inicioEmSegundos = s1 + m1 * 60 + h1 * 3600 + dia1 * 24 * 3600;
+        int fimEmSegundos = s2 + m2 * 60 + h2 * 3600 + dia2 * 24 * 3600;
+
+        int duracao = fimEmSegundos - inicioEmSegundos;
+
+        int dias = duracao / (24 * 3600);
+        duracao %= (24 * 3600);
+
+        int horas = duracao / 3600;
+        duracao %= 3600;
+
+        int minutos = duracao / 60;
+        int segundos = duracao % 60;
+
+        System.out.println(dias + " dia(s)");
+        System.out.println(horas + " hora(s)");
+        System.out.println(minutos + " minuto(s)");
+        System.out.println(segundos + " segundo(s)");
+
+        sc.close();
     }
 }
