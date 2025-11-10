34d578290515596651bff2fc6e566bfca831d353
Eric Vieira Pereira <86498398+ericvpereira@users.noreply.github.com>
2025-11-10 20:39:24 -0300

    Add files via upload

--- Beecrowd/src/BEE2313.java
+++ Beecrowd/src/BEE2313.java
@@ -1,31 +1,43 @@
 import java.util.Scanner;
 
 public class BEE2313 {
-    static void main() {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int A = sc.nextInt();
         int B = sc.nextInt();
         int C = sc.nextInt();
 
+        // Verifica se é triângulo válido
         if (A + B > C && A + C > B && B + C > A) {
-            if (A > B && B == C) {
-                System.out.println("Valido-Isoceles");
+            String tipo;
+
+            // Verifica tipo de triângulo
+            if (A == B && B == C) {
+                tipo = "Valido-Equilatero";
+            } else if (A == B || A == C || B == C) {
+                tipo = "Valido-Isoceles";
+            } else {
+                tipo = "Valido-Escaleno";
+            }
+
+            System.out.println(tipo);
+
+            // Verifica se é retângulo (Pitágoras)
+            int a2 = A * A;
+            int b2 = B * B;
+            int c2 = C * C;
+
+            if (a2 + b2 == c2 || a2 + c2 == b2 || b2 + c2 == a2) {
+                System.out.println("Retangulo: S");
+            } else {
                 System.out.println("Retangulo: N");
             }
-            else if (A == B && B == C) {
-                System.out.println("equilatero");
-            } else if (A == B && A != C) {
-                System.out.println("isosceles");
-                System.out.println("");
-            } else if (A != B && A != C && B != A && B != C) {
-                System.out.println("escaleno");
-            }
-        }
 
-        else {
+        } else {
             System.out.println("Invalido");
         }
 
+        sc.close();
     }
 }
