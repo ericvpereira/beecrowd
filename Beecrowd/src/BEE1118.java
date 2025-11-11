5b55de710f411ca42d25a6a8f8ac5eac4d6fe513
Eric Vieira Pereira <86498398+ericvpereira@users.noreply.github.com>
2025-11-11 20:31:36 -0300

    Add files via upload

--- Beecrowd/src/BEE1118.java
+++ Beecrowd/src/BEE1118.java
@@ -3,18 +3,37 @@ import java.util.Scanner;
 
 public class BEE1118 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        double nota1, nota2, media;
+        int opcao;
 
-        double N1 = entrada.nextDouble();
-        double N2 = entrada.nextDouble();
-        int contador = 0;
+        do {
+            nota1 = lerNotaValida(sc);
+            nota2 = lerNotaValida(sc);
+            media = (nota1 + nota2) / 2.0;
+            System.out.printf("media = %.2f%n", media);
 
-        while (N1 >= 0 && N1 <= 10 && N2 >= 0 && N2 <= 10) {
-            double media = (N1 + N2) / 2;
-            System.out.println("media = " + media);
-            System.out.println("novo calculo (1-sim 2-nao)");
-            contador++;
-        }
-        System.out.println("nota invalida");
+            do {
+                System.out.println("novo calculo (1-sim 2-nao)");
+                opcao = sc.nextInt();
+            } while (opcao != 1 && opcao != 2);
+
+        } while (opcao == 1);
+
+        sc.close();
     }
+
+    private static double lerNotaValida(Scanner sc) {
+        double nota;
+        while (true) {
+            nota = sc.nextDouble();
+            if (nota >= 0 && nota <= 10) {
+                return nota;
+            } else {
+                System.out.println("nota invalida");
+            }
+        }
+    }
+
 }
+
